61a0ee3497fd5c12dca94c2c43d8d6bdd5cd5d37
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-10-23 13:23:43 +0300

    Add nosto js and meta tags

---
+++ Block/Embed.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Nosto\Tagging\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Nosto\Tagging\Helper\Account;
+use Nosto\Tagging\Helper\Data;
+
+require_once 'app/code/Nosto/Tagging/vendor/nosto/php-sdk/autoload.php';
+
+/**
+ * Embed script block that includes the Nosto script in the page <head>.
+ * This block should be included on all pages.
+ */
+class Embed extends Template
+{
+    /**
+     * The default Nosto server address to use if none is configured.
+     */
+    const DEFAULT_SERVER_ADDRESS = 'connect.nosto.com';
+
+    /**
+     * @inheritdoc
+     */
+    protected $_template = 'embed.phtml';
+
+    /**
+     * Constructor.
+     *
+     * @param Context $context the context.
+     * @param Account $accountHelper the account helper.
+     * @param Data    $dataHelper the data helper.
+     * @param array   $data optional data.
+     */
+    public function __construct(
+        Context $context,
+        Account $accountHelper,
+        Data $dataHelper,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+
+        $this->_accountHelper = $accountHelper;
+        $this->_dataHelper = $dataHelper;
+    }
+
+    /**
+     * Returns the account name for the current store.
+     *
+     * @return string the account name or empty string if account is not found.
+     */
+    public function getAccountName()
+    {
+        $store = $this->_storeManager->getStore(true);
+        $account = $this->_accountHelper->findAccount($store);
+		return !is_null($account) ? $account->getName() : '';
+    }
+
+    /**
+     * Returns the Nosto server address.
+     * This is taken from the local environment if it is set, or else it
+     * defaults to "connect.nosto.com".
+     *
+     * @return string the url.
+     */
+    public function getServerAddress()
+    {
+        return isset($_ENV['NOSTO_SERVER_URL'])
+            ? $_ENV['NOSTO_SERVER_URL']
+            : self::DEFAULT_SERVER_ADDRESS;
+    }
+
+    /**
+     * Return if the Nosto script should be included directly.
+     *
+     * @return bool
+     */
+    public function useScriptDirectInclude()
+    {
+        return $this->_dataHelper->getUseScriptDirectInclude();
+    }
+}
---
+++ Block/Meta.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Nosto\Tagging\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Nosto\Tagging\Helper\Data as DataHelper;
+
+require_once 'app/code/Nosto/Tagging/vendor/nosto/php-sdk/autoload.php';
+
+/**
+ * Meta data block for outputting <meta> elements in the page <head>.
+ * This block should be included on all pages.
+ */
+class Meta extends Template
+{
+	/**
+	 * @inheritdoc
+	 */
+	protected $_template = 'meta.phtml';
+
+	/**
+	 * @var DataHelper the module data helper.
+	 */
+	protected $_dataHelper;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Context    $context the context.
+	 * @param DataHelper $dataHelper the data helper.
+	 * @param array      $data optional data.
+	 */
+	public function __construct(
+		Context $context,
+		DataHelper $dataHelper,
+		array $data = []
+	) {
+		parent::__construct($context, $data);
+
+		$this->_dataHelper = $dataHelper;
+	}
+
+	/**
+	 * Returns the module version number.
+	 *
+	 * @return string the module version number.
+	 */
+	public function getModuleVersion()
+	{
+		// todo
+		return 'todo';
+	}
+
+	/**
+	 * Returns the unique installation ID.
+	 *
+	 * @return string the unique ID.
+	 */
+	public function getInstallationId()
+	{
+		return $this->_dataHelper->getInstallationId();
+	}
+
+	/**
+	 * Returns the current stores language code in ISO 639-1 format.
+	 *
+	 * @return string the language code.
+	 */
+	public function getLanguageCode()
+	{
+		$store = $this->_storeManager->getStore(true);
+		return substr($store->getConfig('general/locale/code'), 0, 2);
+	}
+}
