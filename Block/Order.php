b5915fcca76bdf8669644ea84d7a5ab1de7787d8
Mridang Agarwalla <mridang@nosto.com>
2016-01-27 13:25:51 +0200

    Added the order confirmation page tagging.

---
+++ Block/Order.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Nosto\Tagging\Block;
+
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template;
+use Magento\Sales\Model\OrderFactory;
+use Nosto\Tagging\Helper\Format;
+use Nosto\Tagging\Model\Order\Builder as OrderBuilder;
+use NostoPrice;
+
+/** @noinspection PhpIncludeInspection */
+require_once 'app/code/Nosto/Tagging/vendor/nosto/php-sdk/autoload.php';
+
+/**
+ * Category block used for outputting meta-data on the stores category pages.
+ * This meta-data is sent to Nosto via JavaScript when users are browsing the
+ * pages in the store.
+ */
+class Order extends \Magento\Checkout\Block\Success
+{
+    /**
+     * @inheritdoc
+     */
+    protected $_template = 'order.phtml';
+
+    /**
+     * @var OrderBuilder the order meta model builder.
+     */
+    protected $_orderBuilder;
+
+    /**
+     * @var Registry the framework registry.
+     */
+    protected $_registry;
+
+    /**
+     * @var Format the format helper.
+     */
+    protected $_formatHelper;
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $_checkoutSession;
+
+    /**
+     * Constructor.
+     *
+     * @param Template\Context $context
+     * @param OrderFactory $orderFactory
+     * @param OrderBuilder $orderBuilder
+     * @param Format $formatHelper
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param array $data
+     * @internal param Registry $registry
+     * @internal param CategoryBuilder $categoryBuilder
+     */
+    public function __construct(
+        Template\Context $context,
+        OrderFactory $orderFactory,
+        OrderBuilder $orderBuilder,
+        Format $formatHelper,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $orderFactory,
+            $data
+        );
+
+        $this->_formatHelper = $formatHelper;
+        $this->_checkoutSession = $checkoutSession;
+        $this->_orderBuilder = $orderBuilder;
+    }
+
+    /**
+     * Returns the Nosto order meta-data model.
+     *
+     * @return \NostoOrder the order meta data model.
+     */
+    public function getNostoOrder()
+    {
+        /** @var \Magento\Sales\Model\Order $order */
+        return $this->_orderBuilder->build($this->_checkoutSession->getLastRealOrder());
+    }
+
+    /**
+     * Formats a \NostoPrice object, e.g. "1234.56".
+     *
+     * @param NostoPrice $price the price to format.
+     * @return string the formatted price.
+     */
+    public function formatNostoPrice(NostoPrice $price)
+    {
+        return $this->_formatHelper->formatPrice($price);
+    }
+
+    /**
+     * Formats a \NostoDate object, e.g. "2015-12-24";
+     *
+     * @param \NostoDate $date the date to format.
+     * @return string the formatted date.
+     */
+    public function formatNostoDate(\NostoDate $date)
+    {
+        return $this->_formatHelper->formatDate($date);
+    }
+}
