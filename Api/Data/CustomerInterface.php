5e91d6d5ab00b7615738ca14e153a51e2646e8ce
Hannu Pölönen <hannu.polonen@nosto.com>
2016-02-25 10:54:57 +0200

    Update copyrights and composer.json

--- Api/Data/CustomerInterface.php
+++ Api/Data/CustomerInterface.php
@@ -1,12 +1,32 @@
 <?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category  Nosto
+ * @package   Nosto_Tagging
+ * @author    Nosto Solutions Ltd <magento@nosto.com>
+ * @copyright Copyright (c) 2013-2016 Nosto Solutions Ltd (http://www.nosto.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 namespace Nosto\Tagging\Api\Data;
 
-/**
- * Created by PhpStorm.
- * User: hannupolonen
- * Date: 22/02/16
- * Time: 11:29
- */
 interface CustomerInterface
 {
     const CUSTOMER_ID   = 'customer_id';
@@ -34,7 +54,7 @@ interface CustomerInterface
     public function getNostoId();
 
     /**
-     * Get created at time 
+     * Get created at time
      * @return \DateTime
      */
     public function getCreatedAt();
@@ -74,5 +94,4 @@ interface CustomerInterface
      * @param \DateTime $updatedAt
      */
     public function setUpdatedAt(\DateTime $updatedAt);
-
 }
