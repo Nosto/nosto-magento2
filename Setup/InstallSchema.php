decb9ebe226d626a5e30036fc662cedf90c019be
Mridang Agarwalla <mridang@Mridangs-MacBook-Pro.local>
2017-01-25 16:06:31 +0200

    Made fixes to comply with the ECG validation

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -52,10 +52,7 @@ class InstallSchema implements InstallSchemaInterface
      * @param ModuleContextInterface $context
      * @return void
      */
-    public function install(
-        SchemaSetupInterface $setup,
-        ModuleContextInterface $context
-    ) // @codingStandardsIgnoreLine
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context) // @codingStandardsIgnoreLine
     {
         $installer = $setup;
         $installer->startSetup();
