364bddde945aeb1bfdf53d2b9f8fc0594695dd13
Mridang Agarwalla <mridang@nosto.com>
2017-04-12 14:38:41 -0400

    Minor spelling corrections and inspection fixes.

--- CustomerData/CartTagging.php
+++ CustomerData/CartTagging.php
@@ -44,6 +44,7 @@ use Magento\Store\Model\StoreManagerInterface;
 use Nosto\Object\Cart\LineItem;
 use Nosto\Tagging\Model\Cart\Builder as NostoCartBuilder;
 use Nosto\Tagging\Model\Customer as NostoCustomer;
+use Nosto\Tagging\Model\Customer;
 use Nosto\Tagging\Model\CustomerFactory as NostoCustomerFactory;
 use Psr\Log\LoggerInterface;
 
@@ -158,8 +159,8 @@ class CartTagging implements SectionSourceInterface
                 ->setPageSize(1)
                 ->setCurPage(1);
 
-            /** @noinspection PhpUndefinedMethodInspection */
-            $nostoCustomer = $customerQuery->getFirstItem(); // @codingStandardsIgnoreLine
+            /** @var Customer $nostoCustomer */
+            $nostoCustomer = $customerQuery->getFirstItem();
             if ($nostoCustomer->hasData(NostoCustomer::CUSTOMER_ID)) {
                 $nostoCustomer->setUpdatedAt(self::getNow());
             } else {
