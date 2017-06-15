14732da047f103eb01319c3c21a2b2c3fab5c6f3
Chen Liangde <chen.liangde@nosto.com>
2017-06-15 11:12:15 +0300

    Fix typo

--- Helper/Data.php
+++ Helper/Data.php
@@ -108,12 +108,12 @@ class Data extends AbstractHelper
     const XML_PATH_TAG1 = 'nosto/attributes/tag1';
 
     /**
-     * Path to the configuration object for customized tag1
+     * Path to the configuration object for customized tag2
      */
     const XML_PATH_TAG2 = 'nosto/attributes/tag2';
 
     /**
-     * Path to the configuration object for customized tag1
+     * Path to the configuration object for customized tag3
      */
     const XML_PATH_TAG3 = 'nosto/attributes/tag3';
 
@@ -336,7 +336,7 @@ class Data extends AbstractHelper
     }
 
     /**
-     * Get tag1 mapping attributes
+     * Get tag2 mapping attributes
      *
      * @param StoreInterface $store the store model or null.
      * @return array of attributes
@@ -347,7 +347,7 @@ class Data extends AbstractHelper
     }
 
     /**
-     * Get tag1 mapping attributes
+     * Get tag3 mapping attributes
      *
      * @param StoreInterface $store the store model or null.
      * @return array of attributes
