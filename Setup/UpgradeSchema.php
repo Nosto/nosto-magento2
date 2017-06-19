45932161fa2bfb2983d8a72205be5d6ccd5dec04
Chen Liangde <chen.liangde@nosto.com>
2017-06-19 10:11:11 +0300

    Add database schema upgrade script

---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Mastering\SampleModule\Setup;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Nosto\Tagging\Api\Data\CustomerInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '2.1.0', '<')) {
+            $setup->getConnection()->addColumn(
+                $setup->getTable('nosto_tagging_customer'),
+                'CustomerInterface::RESTORE_CART_HASH',
+                [
+                    'type' => Table::TYPE_TEXT,
+                    'nullable' => true,
+                    'comment' => 'Restore cart hash',
+                    'size' => CustomerInterface::NOSTO_TAGGING_RESTORE_CART_ATTRIBUTE_LENGTH
+                ]
+            );
+        }
+
+        $setup->endSetup();
+    }
+}
