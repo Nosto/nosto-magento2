218515d5afe06d5c2702ee390c7118011aa75ac1
Chen Liangde <chen.liangde@nosto.com>
2017-12-20 13:43:37 +0200

    Fix phpcs issue

--- Helper/Url.php
+++ Helper/Url.php
@@ -381,9 +381,7 @@ class Url extends AbstractHelper
      */
     public function getAdminNostoConfigurationUrl(Store $store)
     {
-        $params = array(
-            self::MAGENTO_URL_OPTION_STORE_ID => $store->getStoreId()
-        );
+        $params = [self::MAGENTO_URL_OPTION_STORE_ID => $store->getStoreId()];
         $url = $this->backendDataHelper->getUrl(self::URL_PATH_NOSTO_CONFIG, $params);
 
         return $url;
