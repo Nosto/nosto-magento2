4435ac1a59506264258423d718aa54cac6d640c1
Hannu Pölönen <hannu.polonen@nosto.com>
2019-05-02 14:59:51 +0300

    Add first example tests

---
+++ Test/Integration/Helper/UrlTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nosto\Tagging\Test\Integration\Helper;
+
+use Nosto\Tagging\Test\Integration\TestCase;
+
+class UrlTest extends TestCase
+{
+    /**
+     * Tests generating a category preview URL
+     */
+    public function testActiveDomain()
+    {
+        // ToDo - test generating some real URLS
+        $this->assertEquals('jee', 'jee');
+    }
+}
