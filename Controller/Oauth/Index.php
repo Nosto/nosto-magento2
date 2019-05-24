3ed19a218dc5761ba00b42a4693e03269c63e3e1
Olsi Qose <olsi.qose@nosto.com>
2019-05-24 16:00:14 +0300

    Get current store from store code

--- Controller/Oauth/Index.php
+++ Controller/Oauth/Index.php
@@ -62,8 +62,10 @@ class Index extends Action
     private $nostoHelperScope;
     private $nostoHelperCache;
     private $storeRepository;
+    private $request;
 
     /**
+     * Index constructor.
      * @param Context $context
      * @param NostoLogger $logger
      * @param NostoHelperScope $nostoHelperScope
@@ -92,6 +94,7 @@ class Index extends Action
         $this->nostoHelperScope = $nostoHelperScope;
         $this->nostoHelperCache = $nostoHelperCache;
         $this->storeRepository = $storeRepository;
+        $this->request = $context->getRequest();
     }
 
     /**
@@ -131,7 +134,14 @@ class Index extends Action
     public function save(AccountInterface $account)
     {
         $stores = $this->storeRepository->getList();
-        $currentStore = $this->nostoHelperScope->getStore();
+        $storeCode = $this->request->getParam('___store');
+
+        if ($storeCode !== null) {
+            $currentStore = $this->nostoHelperScope->getStoreByCode($storeCode);
+        } else {
+            $currentStore = $this->nostoHelperScope->getStore();
+        }
+
         /** @var \Magento\Store\Model\Store $store */
         foreach ($stores as $store) {
             $existingAccount = $this->nostoHelperAccount->findAccount($store);
@@ -151,7 +161,7 @@ class Index extends Action
 
         $success =  $this->nostoHelperAccount->saveAccount(
             $account,
-            $this->nostoHelperScope->getStore()
+            $currentStore
         );
 
         // Invalidate cache after reconnected nosto account
