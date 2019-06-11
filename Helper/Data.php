7fe9aaefb370f70befb35d1cbc85da5245553105
Olsi Qose <olsi.qose@nosto.com>
2019-06-11 13:11:59 +0300

    Revert "Merge pull request #472 from Nosto/feature/send-order-with-cust-reference"
    This reverts commit e17fe0779cdb971205b9e7076ffc78fe095540cf, reversing
    changes made to 011005910b497357d34a5dd98062e8c19dfd74c8.

--- Helper/Data.php
+++ Helper/Data.php
@@ -140,6 +140,7 @@ class Data extends AbstractHelper
     const XML_PATH_INDEXER_MEMORY = 'nosto/flags/indexer_memory';
 
     /**
+
      * Path to the configuration object that stores category sorting
      */
     const XML_PATH_CATEGORY_SORTING = 'nosto/flags/category_sorting';
@@ -148,11 +149,6 @@ class Data extends AbstractHelper
      * Path to the configuration object for tagging the date a product has beed added to Magento's catalog
      */
     const XML_PATH_TAG_DATE_PUBLISHED = 'nosto/flags/tag_date_published';
-
-    /**
-     * Path to the configuration object that stores customer reference
-     */
-    const XML_PATH_TRACK_MULTI_CHANNEL_ORDERS = 'nosto/flags/track_multi_channel_orders';
 
     /**
      * Path to the configuration object for pricing variations
@@ -441,17 +437,6 @@ class Data extends AbstractHelper
     public function isCategorySortingEnabled(StoreInterface $store = null)
     {
         return (bool)$this->getStoreConfig(self::XML_PATH_CATEGORY_SORTING, $store);
-    }
-
-    /**
-     * Returns if orders want to be tracked from various channels
-     *
-     * @param StoreInterface|null $store
-     * @return bool
-     */
-    public function isMultiChannelOrderTrackingEnabled(StoreInterface $store = null)
-    {
-        return (bool)$this->getStoreConfig(self::XML_PATH_TRACK_MULTI_CHANNEL_ORDERS, $store);
     }
 
     /**
