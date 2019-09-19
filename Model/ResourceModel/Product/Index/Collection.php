61617658ac197bf2277249e3c308a0a388f62ff0
Cid Lopes <alannettto@gmail.com>
2019-09-19 23:46:01 +0300

    Implement get products by id and store id in index repository

--- Model/ResourceModel/Product/Index/Collection.php
+++ Model/ResourceModel/Product/Index/Collection.php
@@ -81,6 +81,18 @@ class Collection extends AbstractCollection
     }
 
     /**
+     * @param array $ids
+     * @return Collection
+     */
+    public function addProductIdsFilter(array $ids)
+    {
+        return $this->addFieldToFilter(
+            Index::PRODUCT_ID,
+            ['in' => $ids]
+        );
+    }
+
+    /**
      * Filters collection for items that are either dirty or out of sync with Nosto
      * @return Collection
      */
