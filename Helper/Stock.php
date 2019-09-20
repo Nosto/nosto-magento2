8a79a264bc69c62265b365c28c3d0480ab0dca73
Hannu Pölönen <hannu.polonen@nosto.com>
2019-09-20 16:44:52 +0300

    Refactor the price helper to use fast lookups and avoid building full products

--- Helper/Stock.php
+++ Helper/Stock.php
@@ -40,7 +40,6 @@ use Magento\Bundle\Model\Product\Type as Bundled;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Type as ProductType;
 use Magento\Catalog\Model\ProductFactory;
-use Magento\CatalogInventory\Api\StockStateInterface;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\App\Helper\AbstractHelper;
 use Magento\Framework\App\Helper\Context;
@@ -52,30 +51,32 @@ use Nosto\Tagging\Model\Stock\StockRegistryProvider;
  */
 class Stock extends AbstractHelper
 {
+    const MAX_CACHED_ITEMS = 1000;
+
+    /** @var ProductFactory */
     private $productFactory;
-    private $stockItem;
 
     /** @var StockRegistryProvider */
     private $stockRegistryProvider;
+
+    private $runTimeCache = [];
 
     /**
      * Constructor.
      *
      * @param Context $context the context.
      * @param ProductFactory $productFactory
-     * @param StockStateInterface $stockItem
+     * @param StockRegistryProvider $stockRegistryProvider
      */
     public function __construct(
         Context $context,
         ProductFactory $productFactory,
-        StockStateInterface $stockItem,
         StockRegistryProvider $stockRegistryProvider
 
     ) {
         parent::__construct($context);
 
         $this->productFactory = $productFactory;
-        $this->stockItem = $stockItem;
         $this->stockRegistryProvider = $stockRegistryProvider;
     }
 
@@ -96,35 +97,32 @@ class Stock extends AbstractHelper
                 /** @var Bundled $productType */
                 $productType = $product->getTypeInstance();
                 $bundledItemIds = $productType->getChildrenIds($product->getId(), $required = true);
-                $products = [];
+                $productIds = [];
                 foreach ($bundledItemIds as $variants) {
                     if (is_array($variants) && count($variants) > 0) { // @codingStandardsIgnoreLine
-                        foreach ($variants as $variantId) {
-                            /* @var Product $productModel */
-                            /** @noinspection PhpDeprecationInspection */
-                            $productModel = $this->productFactory->create()->load($variantId); // @codingStandardsIgnoreLine
-                            $products[] = $productModel;
+                        foreach ($variants as $productId) {
+                            $productIds[] = $productId;
                         }
                     }
                 }
-                $qty = $this->getMinQty($products);
+                $qty = $this->getMinQty($productIds);
                 break;
             case Grouped::TYPE_CODE:
                 $productType = $product->getTypeInstance();
                 if ($productType instanceof Grouped) {
-                    $products = $productType->getAssociatedProducts($product);
+                    $products = $productType->getAssociatedProductIds($product);
                     $qty = $this->getMinQty($products);
                 }
                 break;
             case Configurable::TYPE_CODE:
                 $productType = $product->getTypeInstance();
                 if ($productType instanceof Configurable) {
-                    $productsIds = $productType->getChildrenIds($product);
-                    $qty = $this->getQtySum($productsIds);
+                    $productIds = $productType->getChildrenIds($product->getId());
+                    $qty = $this->getQtySum($productIds);
                 }
                 break;
             default:
-                $qty += $this->stockItem->getStockQty($product->getId());
+                $qty += $this->getQtyForSingleProduct($product);
                 break;
         }
 
@@ -132,24 +130,70 @@ class Stock extends AbstractHelper
     }
 
     /**
+     * @param Product $product
+     * @return int|null
+     */
+    private function getQtyForSingleProduct(Product $product)
+    {
+        $qtyFromCache = $this->getQtyFromCache($product->getId());
+        if ($qtyFromCache !== null) {
+            return $qtyFromCache;
+        }
+        $stockStatus = $this->stockRegistryProvider->getStockStatus(
+            $product->getId(),
+            StockRegistryProvider::DEFAULT_STOCK_SCOPE
+        );
+        $qty = $stockStatus->getQty();
+        $this->saveQtyToCache($product->getId(), $qty);
+        return $qty;
+    }
+
+    /**
+     * @param int $productId
+     * @param $qty
+     */
+    private function saveQtyToCache($productId, $qty)
+    {
+        $this->runTimeCache[$productId] = $qty;
+        if (count($this->runTimeCache) >= self::MAX_CACHED_ITEMS) {
+            foreach ($this->runTimeCache as $key => $val) {
+                unset($this->runTimeCache[$key]);
+                break;
+            }
+        }
+    }
+
+    /**
+     * @param int $productId
+     * @return int|null
+     */
+    private function getQtyFromCache($productId)
+    {
+        if (!isset($this->runTimeCache[$productId])) {
+            return null;
+        }
+        return $this->runTimeCache[$productId];
+    }
+
+    /**
      * Searches the minimum quantity from the products collection
      *
-     * @param array|Product[] $productCollection
+     * @param int[] $productCollection
      * @return int|mixed
      */
-    private function getMinQty(array $productCollection)
+    private function getMinQty(array $productIds)
     {
         $quantities = [];
+        $stockItems = $this->stockRegistryProvider->getStockStatuses($productIds);
         $minQty = 0;
         /* @var Product $product */
-        foreach ($productCollection as $product) {
-            $quantities[] = $this->getQty($product);
+        foreach ($stockItems->getItems() as $stockItem) {
+            $quantities[] = $stockItem->getQty();
         }
         if (!empty($quantities)) {
             rsort($quantities, SORT_NUMERIC);
             $minQty = array_pop($quantities);
         }
-
         return $minQty;
     }
 
@@ -165,6 +209,7 @@ class Stock extends AbstractHelper
         $stockItems = $this->stockRegistryProvider->getStockStatuses($productIds);
         foreach ($stockItems->getItems() as $item) {
             $qty += $item->getQty();
+            $this->saveQtyToCache($item->getProductId(), $item->getQty());
         }
         return $qty;
     }
--- Model/Stock/StockRegistryProvider.php
+++ Model/Stock/StockRegistryProvider.php
@@ -1,16 +1,19 @@
 <?php
 namespace Nosto\Tagging\Model\Stock;
 
+use Magento\CatalogInventory\Api\Data\StockStatusCollectionInterface as StockStatusCollectionInterface;
 use Magento\CatalogInventory\Model\StockRegistryProvider as MagentoStockRegistryProvider;
 
 class StockRegistryProvider extends MagentoStockRegistryProvider
 {
+    const DEFAULT_STOCK_SCOPE = 0;
+
     /**
      * @param int[] $productIds
      * @param int $scopeId
-     * @return \Magento\CatalogInventory\Api\Data\StockStatusCollectionInterface
+     * @return StockStatusCollectionInterface
      */
-    public function getStockStatuses(array $productIds, $scopeId = 0)
+    public function getStockStatuses(array $productIds, $scopeId = self::DEFAULT_STOCK_SCOPE)
     {
         $criteria = $this->stockStatusCriteriaFactory->create();
         $criteria->setProductsFilter($productIds);
