1bbdfe0b08c4250189e850ce47e67dcd0544e8f5
Cid Lopes <alannettto@gmail.com>
2020-07-10 14:01:03 +0300

    Add elastic search container, use rabbitmq with management interface, use port 8081, mount MySQL volume locally for better performance

--- .docker/scripts/init-and-run.sh
+++ .docker/scripts/init-and-run.sh
@@ -5,6 +5,7 @@ set -m
 
 if [ ! -f /var/www/html/magento2/.installed ]; then
   cd /var/www/html/magento2/ || exit
+  composer config --global repos.packagist composer https://packagist.org
   composer require nosto/php-sdk:@stable
 
   until mysql -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -h"${MYSQL_HOST}"; do
@@ -56,11 +57,37 @@ if [ ! -f /var/www/html/magento2/.installed ]; then
   bin/magento config:set dev/css/merge_css_files 1
   bin/magento config:set dev/css/minify_files 1
 
+  # ElasticSearch Specifics NOTE: CMP doesn't find products with ES enabled.
+  bin/magento config:set catalog/frontend/list_allow_all 0 &
+  bin/magento config:set catalog/frontend/flat_catalog_product 0 &
+  bin/magento config:set catalog/productalert_cron/frequency "D" &
+  bin/magento config:set crontab/default/jobs/catalog_product_alert/schedule/cron_expr "0 0 * * *" &
+  bin/magento config:set catalog/productalert_cron/time "00,00,00" &
+  bin/magento config:set catalog/price/scope "0" &
+  bin/magento config:set catalog/search/enable_eav_indexer "1" &
+  bin/magento config:set catalog/search/engine "elasticsearch6" &
+  bin/magento config:set catalog/search/elasticsearch6_server_hostname "${ES_HOST}" &
+  bin/magento config:set catalog/search/elasticsearch6_server_port "9200" &
+  bin/magento config:set catalog/search/elasticsearch6_index_prefix "magento2" &
+  bin/magento config:set catalog/search/elasticsearch6_enable_auth "0" &
+  bin/magento config:set catalog/search/elasticsearch6_server_timeout "15" &
+  bin/magento config:set catalog/search/search_recommendations_enabled "1" &
+  bin/magento config:set catalog/search/search_recommendations_count "5" &
+  bin/magento config:set catalog/search/search_recommendations_count_results_enabled "0" &
+  bin/magento config:set catalog/search/search_suggestion_enabled "1" &
+  bin/magento config:set catalog/search/search_suggestion_count "2" &
+  bin/magento config:set catalog/search/search_suggestion_count_results_enabled "0" &
+  bin/magento config:set catalog/downloadable/shareable "0" &
+  bin/magento config:set catalog/downloadable/content_disposition "inline" &
+  bin/magento config:set catalog/custom_options/use_calendar "0" &
+
+  wait
+
   # Indexer configuration
   bin/magento indexer:set-mode schedule
 
+  bin/magento cache:enable
   bin/magento cache:flush
-#  bin/magento cache:enable
 
   touch pub/static/deployed_version.txt
   touch /var/www/html/magento2/.installed
