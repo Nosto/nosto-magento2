c0d1ea4eb1f98a3a70e7e5f89a049b123add71ed
Cid Lopes <alannettto@gmail.com>
2022-07-05 13:07:14 +0300

    Schema patch to Nullable

---
+++ Setup/Patch/Schema/MakeCustomerIdNullable.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Copyright (c) 2020, Nosto Solutions Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Nosto Solutions Ltd <contact@nosto.com>
+ * @copyright 2020 Nosto Solutions Ltd
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ *
+ */
+
+namespace Nosto\Tagging\Setup\Patch\Schema;
+
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\Patch\SchemaPatchInterface;
+use Nosto\Tagging\Model\ResourceModel\Customer as NostoCustomer;
+use Nosto\Tagging\Api\Data\CustomerInterface;
+
+class MakeCustomerIdNullable implements SchemaPatchInterface
+{
+    /** @var SchemaSetupInterface */
+    private SchemaSetupInterface $schemaSetup;
+
+    /**
+     * MakeCustomerIdNullable constructor.
+     * @param SchemaSetupInterface $schemaSetup
+     */
+    public function __construct(
+        SchemaSetupInterface $schemaSetup
+    )
+    {
+        $this->schemaSetup = $schemaSetup;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->schemaSetup->startSetup();
+        $connection = $this->schemaSetup->getConnection();
+        $connection->modifyColumn(
+            NostoCustomer::TABLE_NAME,
+            CustomerInterface::CUSTOMER_ID,
+            [
+                'type' => 'integer',
+                'name' => CustomerInterface::CUSTOMER_ID,
+                'unsigned' => true,
+                'nullable' => true,
+                'identity' => false,
+                'comment' => 'Customer ID'
+            ]
+        );
+        $this->schemaSetup->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
