cad36aec809237f3c8fad684ca44e638bb4f412f
Cid Lopes <alannettto@gmail.com>
2022-07-08 09:04:05 +0300

    Fix call to undefined warn method

--- Observer/Order/Save.php
+++ Observer/Order/Save.php
@@ -273,7 +273,7 @@ class Save implements ObserverInterface
                 );
             }
         } else {
-            $this->logger->warn(
+            $this->logger->warning(
                 sprintf(
                     'Could not resolve Nosto customer id for order #%s',
                     $order->getQuoteId()
