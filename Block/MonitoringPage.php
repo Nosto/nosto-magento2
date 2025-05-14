906485517ab2534c57de26f8aeb808b520b92eec
Bojan <bojan.mihajlovic@soprex.com>
2025-05-14 16:20:02 +0200

    Improve log files download

--- Block/MonitoringPage.php
+++ Block/MonitoringPage.php
@@ -4,14 +4,9 @@ namespace Nosto\Tagging\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Backend\Block\Template\Context;
-use ZipArchive;
 
 class MonitoringPage extends Template
 {
-    private const string LOG_LOCATION = BP . '/var/log/';
-
-    private const string ARCHIVE_NAME = 'nosto-logs.zip';
-
     public function __construct(Context $context, array $data = [])
     {
         parent::__construct($context, $data);
@@ -25,38 +20,5 @@ class MonitoringPage extends Template
     public function getDownloadLogFilesFormAction(): string
     {
         return $this->getUrl('nosto/monitoring/logs', ['_secure' => true]);
-
-//        $fileNames = [];
-//
-//        $logFiles = scandir(self::LOG_LOCATION);
-//        foreach ($logFiles as $logFile) {
-//            if (str_starts_with($logFile, 'nosto')) {
-//                $fileNames[] = $logFile;
-//            }
-//        }
-//
-//        $zipName = 'nosto-logs.zip';
-//
-//        $this->compressAndDownloadLogFiles($fileNames, $zipName);
-    }
-
-    private function compressAndDownloadLogFiles(array $files, string $zipFileName): void
-    {
-        $zip = new ZipArchive();
-        if ($zip->open($zipFileName, ZipArchive::CREATE) !== TRUE) {
-            exit('Cannot open ' . $zipFileName);
-        }
-
-        foreach ($files as $file) {
-            $zip->addFile(self::LOG_LOCATION . $file, $file);
-        }
-        $zip->close();
-
-        header("Content-type: application/zip");
-        header("Content-Disposition: attachment; filename = $zipFileName");
-        header("Pragma: no-cache");
-        header("Expires: 0");
-        readfile("$zipFileName");
-        exit;
     }
 }
--- Controller/Monitoring/Logs.php
+++ Controller/Monitoring/Logs.php
@@ -3,6 +3,9 @@
 namespace Nosto\Tagging\Controller\Monitoring;
 
 use Magento\Framework\App\ActionInterface;
+use Magento\Framework\App\Response\RedirectInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Message\ManagerInterface;
 use ZipArchive;
 
 class Logs implements ActionInterface
@@ -10,6 +13,25 @@ class Logs implements ActionInterface
     private const string LOG_LOCATION = BP . '/var/log/';
 
     private const string ARCHIVE_NAME = 'nosto-logs.zip';
+
+    /** @var ManagerInterface $messageManager */
+    private ManagerInterface $messageManager;
+
+    /** @var RedirectFactory $redirectFactory */
+    private RedirectFactory $redirectFactory;
+
+    /** @var RedirectInterface $redirect */
+    private RedirectInterface $redirect;
+
+    public function __construct(
+        ManagerInterface $messageManager,
+        RedirectFactory $redirectFactory,
+        RedirectInterface $redirect
+    ) {
+        $this->messageManager = $messageManager;
+        $this->redirectFactory = $redirectFactory;
+        $this->redirect = $redirect;
+    }
 
     public function execute()
     {
@@ -22,10 +44,16 @@ class Logs implements ActionInterface
             }
         }
 
+        if (!is_readable(self::LOG_LOCATION)) {
+            $this->messageManager->addErrorMessage('Permission denied!');
+
+            return $this->redirectFactory->create()->setUrl($this->redirect->getRefererUrl());
+        }
+
         $this->compressAndDownloadLogFiles($fileNames);
     }
 
-    private function compressAndDownloadLogFiles(array $files): void
+    private function compressAndDownloadLogFiles(array $files)
     {
         $zip = new ZipArchive();
         if ($zip->open(self::ARCHIVE_NAME, ZipArchive::CREATE) !== true) {
@@ -38,7 +66,7 @@ class Logs implements ActionInterface
         $zip->close();
 
         header("Content-type: application/zip");
-        header("Content-Disposition: attachment; filename = " . (self::ARCHIVE_NAME) . "");
+        header("Content-Disposition: attachment; filename = " . (self::ARCHIVE_NAME));
         header("Pragma: no-cache");
         header("Expires: 0");
         readfile(self::ARCHIVE_NAME);
