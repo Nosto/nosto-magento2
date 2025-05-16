57b44539abe10546cb9a860f4aaa6057be3cbb20
Bojan <bojan.mihajlovic@soprex.com>
2025-05-16 12:32:07 +0200

    Added last indexed time to debugger

--- Block/MonitoringPage.php
+++ Block/MonitoringPage.php
@@ -2,8 +2,10 @@
 
 namespace Nosto\Tagging\Block;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\View\Element\Template;
 use Magento\Backend\Block\Template\Context;
+use Magento\Indexer\Model\ResourceModel\Indexer\State\Collection;
 use Magento\Store\Model\Store;
 use Nosto\Model\Signup\Account as NostoAccount;
 use Nosto\Tagging\Helper\Account;
@@ -75,5 +77,17 @@ class MonitoringPage extends Template
     public function getStringSettingsValues(string $method): string
     {
         return $this->settings->$method($this->store);
+    }
+
+    public function getLastIndexedTime(string $indexerName): array
+    {
+        $indexerCollection = ObjectManager::getInstance()
+            ->create(Collection::class);
+        $nostoIndexer = $indexerCollection->getItemByColumnValue('indexer_id', $indexerName);
+
+        return [
+            'indexer_id' => $nostoIndexer->getData('indexer_id'),
+            'updated' => $nostoIndexer->getData('updated')
+        ];
     }
 }
