65aba6fb57ccd83c790a6aa216cc19568ae3b44c
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-07-19 12:46:04 +0300

    NS-14370 Discontinue products on website unassignment

--- Plugin/ProductUpdate.php
+++ Plugin/ProductUpdate.php
@@ -37,7 +37,9 @@
 namespace Nosto\Tagging\Plugin;
 
 use Closure;
+use Exception;
 use Magento\Catalog\Model\ResourceModel\Product as MagentoResourceProduct;
+use Magento\Catalog\Model\ResourceModel\Product\Website\Link as ProductWebsiteLink;
 use Magento\Framework\Indexer\IndexerRegistry;
 use Magento\Framework\Model\AbstractModel;
 use Nosto\Tagging\Exception\ParentProductDisabledException;
@@ -74,6 +76,9 @@ class ProductUpdate
     /** @var CollectionBuilder */
     private CollectionBuilder $productCollectionBuilder;
 
+    /** @var ProductWebsiteLink */
+    private ProductWebsiteLink $productWebsiteLink;
+
     /**
      * ProductUpdate constructor.
      * @param IndexerRegistry $indexerRegistry
@@ -82,6 +87,8 @@ class ProductUpdate
      * @param NostoLogger $logger
      * @param ProductUpdateService $productUpdateService
      * @param NostoHelperScope $nostoHelperScope
+     * @param CollectionBuilder $productCollectionBuilder
+     * @param ProductWebsiteLink $productWebsiteLink
      */
     public function __construct(
         IndexerRegistry                $indexerRegistry,
@@ -90,7 +97,8 @@ class ProductUpdate
         NostoLogger                    $logger,
         ProductUpdateService           $productUpdateService,
         NostoHelperScope               $nostoHelperScope,
-        CollectionBuilder              $productCollectionBuilder
+        CollectionBuilder              $productCollectionBuilder,
+        ProductWebsiteLink             $productWebsiteLink
     ) {
         $this->indexerRegistry = $indexerRegistry;
         $this->productIndexer = $productIndexer;
@@ -99,6 +107,7 @@ class ProductUpdate
         $this->productUpdateService = $productUpdateService;
         $this->nostoHelperScope = $nostoHelperScope;
         $this->productCollectionBuilder = $productCollectionBuilder;
+        $this->productWebsiteLink = $productWebsiteLink;
     }
 
     /**
@@ -119,7 +128,63 @@ class ProductUpdate
             });
         }
 
+        // A product removed from a website disappears from the store-filtered indexer
+        // collections, so the update pipeline can never mark it discontinued for the
+        // stores it left. Capture the persisted website assignments before the save
+        // and diff them against the database once the transaction has committed.
+        $websiteIdsBeforeSave = $this->getPersistedWebsiteIds($product);
+        if (!empty($websiteIdsBeforeSave)) {
+            $productResource->addCommitCallback(
+                function () use ($product, $websiteIdsBeforeSave) {
+                    $this->queueDiscontinueForRemovedWebsites($product, $websiteIdsBeforeSave);
+                }
+            );
+        }
+
         return $proceed($product);
+    }
+
+    /**
+     * Returns the website ids currently persisted for the product in catalog_product_website
+     *
+     * @param AbstractModel $product
+     * @return int[]
+     */
+    private function getPersistedWebsiteIds(AbstractModel $product): array
+    {
+        if (!$product->getId()) {
+            return [];
+        }
+        return array_map('intval', $this->productWebsiteLink->getWebsiteIdsByProductId((int)$product->getId()));
+    }
+
+    /**
+     * Queues a discontinue message for every store view belonging to a website
+     * the product was unassigned from during the save
+     *
+     * @param AbstractModel $product
+     * @param int[] $websiteIdsBeforeSave
+     * @return void
+     */
+    private function queueDiscontinueForRemovedWebsites(
+        AbstractModel $product,
+        array $websiteIdsBeforeSave
+    ): void {
+        try {
+            $websiteIdsAfterSave = array_map(
+                'intval',
+                $this->productWebsiteLink->getWebsiteIdsByProductId((int)$product->getId())
+            );
+            $removedWebsiteIds = array_diff($websiteIdsBeforeSave, $websiteIdsAfterSave);
+            foreach ($removedWebsiteIds as $websiteId) {
+                $stores = $this->nostoHelperScope->getWebsite($websiteId)->getStores();
+                foreach ($stores as $store) {
+                    $this->productUpdateService->addIdsToDeleteMessageQueue([$product->getId()], $store);
+                }
+            }
+        } catch (Exception $e) {
+            $this->logger->exception($e);
+        }
     }
 
     /**
---
+++ Test/Unit/Plugin/ProductWebsiteUpdateTest.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * Copyright (c) 2026, Nosto Solutions Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Nosto Solutions Ltd <contact@nosto.com>
+ * @copyright 2026 Nosto Solutions Ltd
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Nosto\Tagging\Test\Unit\Plugin;
+
+use Magento\Catalog\Model\Product\Action as ProductAction;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\Website;
+use Nosto\Tagging\Helper\Scope as NostoHelperScope;
+use Nosto\Tagging\Logger\Logger as NostoLogger;
+use Nosto\Tagging\Model\Service\Update\ProductUpdateService;
+use Nosto\Tagging\Plugin\ProductWebsiteUpdate;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class ProductWebsiteUpdateTest extends TestCase
+{
+    /** @var ProductWebsiteUpdate */
+    private ProductWebsiteUpdate $plugin;
+
+    /** @var ProductAction|MockObject */
+    private MockObject $productActionMock;
+
+    /** @var ProductUpdateService|MockObject */
+    private MockObject $productUpdateServiceMock;
+
+    /** @var NostoHelperScope|MockObject */
+    private MockObject $nostoHelperScopeMock;
+
+    protected function setUp(): void
+    {
+        $this->productActionMock = $this->createMock(ProductAction::class);
+        $this->productUpdateServiceMock = $this->createMock(ProductUpdateService::class);
+        $this->nostoHelperScopeMock = $this->createMock(NostoHelperScope::class);
+
+        $this->plugin = new ProductWebsiteUpdate(
+            $this->productUpdateServiceMock,
+            $this->nostoHelperScopeMock,
+            $this->createMock(NostoLogger::class)
+        );
+    }
+
+    /**
+     * Builds a website mock exposing the given stores
+     *
+     * @param Store[] $stores
+     * @return Website|MockObject
+     */
+    private function mockWebsite(array $stores): MockObject
+    {
+        $website = $this->createMock(Website::class);
+        $website->method('getStores')->willReturn($stores);
+        return $website;
+    }
+
+    /**
+     * @covers \Nosto\Tagging\Plugin\ProductWebsiteUpdate::afterUpdateWebsites()
+     */
+    public function testMassWebsiteRemovalQueuesDiscontinueForStoresOfRemovedWebsites(): void
+    {
+        $productIds = [11, 22, 33];
+
+        $storeA = $this->createMock(Store::class);
+        $storeB = $this->createMock(Store::class);
+        $this->nostoHelperScopeMock->method('getWebsite')
+            ->willReturnMap([
+                [2, $this->mockWebsite([$storeA])],
+                [7, $this->mockWebsite([$storeB])],
+            ]);
+
+        $deleteQueueCalls = [];
+        $this->productUpdateServiceMock->expects($this->exactly(2))
+            ->method('addIdsToDeleteMessageQueue')
+            ->willReturnCallback(function (array $ids, $store) use (&$deleteQueueCalls) {
+                $deleteQueueCalls[] = [$ids, $store];
+            });
+
+        $this->plugin->afterUpdateWebsites(
+            $this->productActionMock,
+            null,
+            $productIds,
+            [2, 7],
+            'remove'
+        );
+
+        $this->assertSame([$productIds, $storeA], $deleteQueueCalls[0]);
+        $this->assertSame([$productIds, $storeB], $deleteQueueCalls[1]);
+    }
+
+    /**
+     * @covers \Nosto\Tagging\Plugin\ProductWebsiteUpdate::afterUpdateWebsites()
+     */
+    public function testMassWebsiteAdditionDoesNotQueueDiscontinue(): void
+    {
+        $this->productUpdateServiceMock->expects($this->never())
+            ->method('addIdsToDeleteMessageQueue');
+
+        $this->plugin->afterUpdateWebsites(
+            $this->productActionMock,
+            null,
+            [11, 22],
+            [2],
+            'add'
+        );
+    }
+
+    /**
+     * @covers \Nosto\Tagging\Plugin\ProductWebsiteUpdate::afterUpdateWebsites()
+     */
+    public function testEmptyProductOrWebsiteListsDoNothing(): void
+    {
+        $this->productUpdateServiceMock->expects($this->never())
+            ->method('addIdsToDeleteMessageQueue');
+
+        $this->plugin->afterUpdateWebsites($this->productActionMock, null, [], [2], 'remove');
+        $this->plugin->afterUpdateWebsites($this->productActionMock, null, [11], [], 'remove');
+    }
+}
